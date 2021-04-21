ac8320d42088354e7cb555f201f0274ab8c5bc36
armaan10 <armaan.ashfaque@learner.manipal.edu>
2021-04-21 14:52:03 +0530

    tested lookup

--- utils/link.py
+++ utils/link.py
@@ -2,6 +2,10 @@ import torch
 from parser import read_file
 
 
+
+''''size of vector total (vocab,embd_dim)'''
+
+
 def create_dict(vocab):
     #create the vectors + link each
     dic={}
@@ -11,8 +15,25 @@ def create_dict(vocab):
         dic[word]=val
         val+=1
     return dic,len(vocab)
+def lookup(tokens,dicto,vectors):
+    idx_no=[dicto[x] for x in tokens]
+    return vectors[idx_no]
 
 if __name__ == "__main__":
     vocab=read_file("/home/armaan/Desktop/Desktop_Files/transcoderplus/dataset/bpe_files/py_train_vocab")
-    dict_py,_=create_dict(vocab)
-    print(dict_py)
+    dict_py,size=create_dict(vocab)
+    tokens=read_file("/home/armaan/Desktop/Desktop_Files/transcoderplus/dataset/bpe_files/py_train_bpe")
+    tokens_list=[]
+    for i in tokens:
+        
+        tokens_list+=i.split()
+    vectors=torch.rand(size,1)
+    print(vectors[18])
+    print(lookup(tokens_list[:2],dict_py,vectors))
+    
+    '''print(size)
+    
+    print(vectors[[12,]])
+    x=["'",","]
+    print(lookup(tokens[:2],dict_py,vectors))'''
+    
